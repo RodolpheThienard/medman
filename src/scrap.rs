2a5352c9ec0e799aeac535d2283aa76e3223b19a
rodolphethienard <thienard.rodolphe1@gmail.com>
2021-11-20 15:55:32 +0100

    writte scraping data in metadata, end of scrap

--- src/scrap.rs
+++ src/scrap.rs
@@ -2,25 +2,39 @@ use reqwest::Error;
 use serde_json::Value;
 use crate::musicfile::MusicFile;
 use urlencoding::encode;
+use audiotags::Tag;
 
-pub fn scrap(music_files: Vec<MusicFile>) -> Result<i32, Error> {
+pub fn scrap(music_files: Vec<MusicFile>) -> Result<(), Error> {
 
     for music in music_files {
         let split = music.path().split('/').collect::<Vec<&str>>();
         let split_data = split[split.len()-1].split('-'); 
 
-        let encode_artist = encode(split_data.clone().collect::<Vec<&str>>()[0]);
-        let tmp_title = encode(split_data.clone().collect::<Vec<&str>>()[1]);
-        let encode_title = tmp_title.split('.').collect::<Vec<&str>>()[0];
+        
+        let artist = split_data.clone().collect::<Vec<&str>>()[0];
+        let encode_artist = encode(artist);
+
+        let tmp_title = split_data.clone().collect::<Vec<&str>>()[1];
+        let title = tmp_title.split('.').collect::<Vec<&str>>()[0];
+        let encode_title = encode(title);
+
         let url = format!("https://musicbrainz.org/ws/2/recording/?query={} AND artists:{}&fmt=json", encode_title, encode_artist);
         let find_data = reqwest::get(url.as_str())?
             .text()?;
 
         let data: Value = serde_json::from_str(&find_data).unwrap();
         
-        let test = &data["recordings"][0];
+        let album = &data["recordings"][0]["releases"][0]["title"];
+        let year = &data["recordings"][0]["releases"][0]["release-events"][0]["date"];
         
-        println!("body = {:#?}", test);
+        println!("album = {} | date = {}", album, year);
+
+        let mut value = Tag::default().read_from_path(&music.path()).unwrap();
+        value.set_title(title);
+        value.set_year(std::str::FromStr::from_str(&year.to_string()[1..5]).unwrap());
+        value.set_artist(artist);
+        value.set_album_title(&album.as_str().unwrap());
+        value.write_to_path(&music.path()).unwrap();
     }
-    Ok(3)
+    Ok(())
 }
